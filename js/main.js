218e82d2d24b58d0601c5cb9e7f5e61c84ddbcde
sonyakim-dev <sonyakim.dev@gmail.com>
2022-04-16 01:25:57 -0700

    add common css/js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', 'Search');
-});
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const swiper = new Swiper('.slider .swiper', {
   speed: 1000,
   autoplay: true,
